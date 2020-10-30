1cce2e3a8e7a1c52b17f1c58b8d2a21912316540
tbcwebsitecreation <rubinaramesh1973@gmail.com>
2020-10-29 23:01:11 -0700

    header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
+import React from 'react';
+import Header from './components/Header'
 
-import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
-  );
+	return <div className='App'>
+    <Header />
+  </div>;
 }
 
 export default App;
