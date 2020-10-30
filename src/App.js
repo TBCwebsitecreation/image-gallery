f1cda6639f61851ee50f67118239144b65c1e5e7
tbcwebsitecreation <rubinaramesh1973@gmail.com>
2020-10-29 23:08:03 -0700

    search form and search result components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import Header from './components/Header'
-
+import Header from './components/Header';
+import SearchForm from './components/SearchForm';
+import SearchResult from './components/SearchResult';
 
 function App() {
-	return <div className='App'>
-    <Header />
-  </div>;
+	return (
+		<div className='App'>
+			<Header />
+      <SearchForm />
+      <SearchResult />
+		</div>
+	);
 }
 
 export default App;
